c6418fdecd70db2adbe10539b7dff099ebd32390
tommyoguro <tomoharu.oguro.2022@globis.ac.jp>
2024-12-01 22:49:40 +0900

    Initial commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,9 +10,17 @@ class OrdersController < ApplicationController
   end
 
   def create
+    ActiveRecord::Base.transaction do
     @order = current_user.orders.build(order_params)
-    @order.save
+
+    @order.ordered_lists.each do |ordered_list|
+      item = Item.lock('FOR UPDATE').find(ordered_list.item_id)
+  end
+
+    @order.save!
     @order.update_total_quantity
+  end 
+
     # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
     redirect_to orders_path
   end
